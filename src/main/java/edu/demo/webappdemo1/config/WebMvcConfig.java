4501c31dddd93cc1863101ec80335e662b0e7704
Ivan <imataras@yandex.ru>
2022-08-17 03:43:20 +0300

    add spring configuration and jpa configuration

---
+++ src/main/java/edu/demo/webappdemo1/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package edu.demo.webappdemo1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("edu.demo.webappdemo1")
+public class WebMvcConfig {
+
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
