88e8125061290835a6454e1b11b08b61f2d2720b
Ivan <imataras@yandex.ru>
2022-08-29 03:23:13 +0300

    add spring boot configuration

--- src/main/java/net/codejava/config/WebAppInitializer.java
+++ src/main/java/net/codejava/config/WebAppInitializer.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,8 +14,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.scan("net.codejava");
+        servletContext.addListener(new ContextLoaderListener(appContext));
         appContext.register(WebMvcConfig.class);
-
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
